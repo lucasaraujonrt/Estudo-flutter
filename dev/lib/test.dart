064716508af202a79dce621660cda11f6f58a25c
Lucas Araujo e Silva <lucasaraujo8186@gmail.com>
2020-02-25 11:17:00 -0300

    Arquivos Primarios de Flutter

---
+++ dev/lib/test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class bottom extends StatelessWidget {
+  
+  bottom({@required this.onPressed});  
+  
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context){ 
+    return null;
+  }
+
+
+}
